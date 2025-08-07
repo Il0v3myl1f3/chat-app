3710700c08ae7d94106200642d9074b39151b2e9
Trohin Alexandru <64481997+Il0v3myl1f3@users.noreply.github.com>
2025-08-07 14:56:31 +0300

    Update server.c
    Refactored code, removed goto's

--- src/server/server.c
+++ src/server/server.c
@@ -23,7 +23,7 @@ int create_socket(void) {
     }
 
     int opt = 1;
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt)) < 0) {
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *) &opt, sizeof(opt)) < 0) {
         print_error("Failed to set socket options");
         CLOSE_SOCKET(sock);
         return -1;
@@ -38,7 +38,7 @@ int bind_socket(SOCKET sock, int port) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    if (bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+    if (bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         print_error("Failed to bind socket");
         return -1;
     }
@@ -54,9 +54,9 @@ int listen_socket(SOCKET sock, int backlog) {
     return 0;
 }
 
-SOCKET accept_connection(SOCKET sock, struct sockaddr_in* client_addr) {
+SOCKET accept_connection(SOCKET sock, struct sockaddr_in *client_addr) {
     int addr_len = sizeof(*client_addr);
-    SOCKET client_sock = accept(sock, (struct sockaddr*)client_addr, &addr_len);
+    SOCKET client_sock = accept(sock, (struct sockaddr *) client_addr, &addr_len);
     if (client_sock == INVALID_SOCKET) {
         print_error("Failed to accept connection");
         return INVALID_SOCKET;
@@ -64,15 +64,15 @@ SOCKET accept_connection(SOCKET sock, struct sockaddr_in* client_addr) {
     return client_sock;
 }
 
-int send_message(SOCKET sock, const MessageInfo* msg) {
-    return send(sock, (char*)msg, sizeof(MessageInfo), 0);
+int send_message(SOCKET sock, const MessageInfo *msg) {
+    return send(sock, (char *) msg, sizeof(MessageInfo), 0);
 }
 
-int receive_message(SOCKET sock, MessageInfo* msg) {
-    return recv(sock, (char*)msg, sizeof(MessageInfo), 0);
+int receive_message(SOCKET sock, MessageInfo *msg) {
+    return recv(sock, (char *) msg, sizeof(MessageInfo), 0);
 }
 
-int server_init(ServerState* server, int port) {
+int server_init(ServerState *server, int port) {
     memset(server, 0, sizeof(ServerState));
     server->next_client_id = 1;
 
@@ -121,7 +121,7 @@ int server_init(ServerState* server, int port) {
     return 0;
 }
 
-void server_cleanup(ServerState* server) {
+void server_cleanup(ServerState *server) {
     pthread_mutex_lock(&server->clients_mutex);
     for (int i = 0; i < MAX_CLIENTS; i++) {
         if (server->clients[i].active) {
@@ -136,7 +136,7 @@ void server_cleanup(ServerState* server) {
     pthread_mutex_destroy(&server->nicknames_mutex);
 }
 
-int is_nickname_available(ServerState* server, const char* nickname) {
+int is_nickname_available(ServerState *server, const char *nickname) {
     pthread_mutex_lock(&server->nicknames_mutex);
     for (int i = 0; i < MAX_CLIENTS; i++) {
         if (server->nicknames[i].active && strcmp(server->nicknames[i].nickname, nickname) == 0) {
@@ -148,7 +148,7 @@ int is_nickname_available(ServerState* server, const char* nickname) {
     return 1;
 }
 
-int register_nickname(ServerState* server, const char* nickname, int client_id) {
+int register_nickname(ServerState *server, const char *nickname, int client_id) {
     pthread_mutex_lock(&server->nicknames_mutex);
 
     for (int i = 0; i < MAX_CLIENTS; i++) {
@@ -164,7 +164,7 @@ int register_nickname(ServerState* server, const char* nickname, int client_id) 
             server->nicknames[i].client_id = client_id;
             server->nicknames[i].active = 1;
             pthread_mutex_unlock(&server->nicknames_mutex);
-            return 0; // Success
+            return 0;
         }
     }
 
@@ -172,7 +172,7 @@ int register_nickname(ServerState* server, const char* nickname, int client_id) 
     return -1;
 }
 
-void unregister_nickname(ServerState* server, int client_id) {
+void unregister_nickname(ServerState *server, int client_id) {
     pthread_mutex_lock(&server->nicknames_mutex);
     for (int i = 0; i < MAX_CLIENTS; i++) {
         if (server->nicknames[i].active && server->nicknames[i].client_id == client_id) {
@@ -183,7 +183,7 @@ void unregister_nickname(ServerState* server, int client_id) {
     pthread_mutex_unlock(&server->nicknames_mutex);
 }
 
-int find_client_by_nickname(ServerState* server, const char* nickname) {
+int find_client_by_nickname(ServerState *server, const char *nickname) {
     pthread_mutex_lock(&server->clients_mutex);
     for (int i = 0; i < MAX_CLIENTS; i++) {
         if (server->clients[i].active && strcmp(server->clients[i].nickname, nickname) == 0) {
@@ -195,7 +195,7 @@ int find_client_by_nickname(ServerState* server, const char* nickname) {
     return -1;
 }
 
-int add_client(ServerState* server, SOCKET client_socket, struct sockaddr_in client_addr) {
+int add_client(ServerState *server, SOCKET client_socket, struct sockaddr_in client_addr) {
     pthread_mutex_lock(&server->clients_mutex);
 
     if (server->client_count >= MAX_CLIENTS) {
@@ -220,7 +220,7 @@ int add_client(ServerState* server, SOCKET client_socket, struct sockaddr_in cli
     return -1;
 }
 
-void remove_client(ServerState* server, int client_index) {
+void remove_client(ServerState *server, int client_index) {
     pthread_mutex_lock(&server->clients_mutex);
 
     if (client_index >= 0 && client_index < MAX_CLIENTS && server->clients[client_index].active) {
@@ -234,7 +234,7 @@ void remove_client(ServerState* server, int client_index) {
     pthread_mutex_unlock(&server->clients_mutex);
 }
 
-void broadcast_message(ServerState* server, const MessageInfo* msg, int exclude_index) {
+void broadcast_message(ServerState *server, const MessageInfo *msg, int exclude_index) {
     pthread_mutex_lock(&server->clients_mutex);
 
     for (int i = 0; i < MAX_CLIENTS; i++) {
@@ -251,7 +251,7 @@ void broadcast_message(ServerState* server, const MessageInfo* msg, int exclude_
     pthread_mutex_unlock(&server->clients_mutex);
 }
 
-int server_send_private_message(ServerState* server, const MessageInfo* msg) {
+int server_send_private_message(ServerState *server, const MessageInfo *msg) {
     int target_index = find_client_by_nickname(server, msg->target_nickname);
 
     if (target_index == -1) {
@@ -269,13 +269,100 @@ int server_send_private_message(ServerState* server, const MessageInfo* msg) {
 }
 
 typedef struct {
-    ServerState* server;
+    ServerState *server;
     int client_index;
 } client_thread_data_t;
 
-void* handle_client(void* arg) {
-    client_thread_data_t* data = arg;
-    ServerState* server = data->server;
+static int handle_join_message(ServerState *server, int client_index, const MessageInfo *msg) {
+    if (is_nickname_available(server, msg->nickname)) {
+        if (register_nickname(server, msg->nickname, server->clients[client_index].client_id) == 0) {
+            strcpy(server->clients[client_index].nickname, msg->nickname);
+            print_system_message("User joined the chat");
+            printf("Nickname: " CYAN "%s" RESET " (ID: " YELLOW "%d" RESET ")\n",
+                   msg->nickname, server->clients[client_index].client_id);
+
+            MessageInfo success_msg;
+            success_msg.type = MSG_TYPE_NICKNAME_AVAILABLE;
+            strcpy(success_msg.nickname, "Server");
+            sprintf(success_msg.message, "Nickname '%s' is registered!", msg->nickname);
+            success_msg.timestamp = time(NULL);
+            send_message(server->clients[client_index].socket, &success_msg);
+
+            MessageInfo join_msg = *msg;
+            join_msg.type = MSG_TYPE_SYSTEM;
+            sprintf(join_msg.message, "%s joined the chat", msg->nickname);
+            broadcast_message(server, &join_msg, client_index);
+        } else {
+            MessageInfo error_msg;
+            error_msg.type = MSG_TYPE_NICKNAME_TAKEN;
+            strcpy(error_msg.nickname, "Server");
+            strcpy(error_msg.message, "Failed to register nickname. Please try again.");
+            error_msg.timestamp = time(NULL);
+            send_message(server->clients[client_index].socket, &error_msg);
+        }
+    } else {
+        MessageInfo taken_msg;
+        taken_msg.type = MSG_TYPE_NICKNAME_TAKEN;
+        strcpy(taken_msg.nickname, "Server");
+        sprintf(taken_msg.message, "Nickname '%s' is already taken. Please choose another.", msg->nickname);
+        taken_msg.timestamp = time(NULL);
+        send_message(server->clients[client_index].socket, &taken_msg);
+    }
+    return 0;
+}
+
+static int handle_private_message(ServerState *server, int client_index, const MessageInfo *msg) {
+    printf(MAGENTA "[PRIVATE]" RESET " From " CYAN "%s" RESET " to " CYAN "%s" RESET ": %s\n",
+           msg->nickname, msg->target_nickname, msg->message);
+
+    if (server_send_private_message(server, msg) != 0) {
+        MessageInfo error_msg;
+        error_msg.type = MSG_TYPE_SYSTEM;
+        strcpy(error_msg.nickname, "Server");
+        sprintf(error_msg.message, "User '%s' not found or offline.", msg->target_nickname);
+        error_msg.timestamp = time(NULL);
+        send_message(server->clients[client_index].socket, &error_msg);
+    }
+    return 0;
+}
+
+static int handle_leave_message(ServerState *server, int client_index, const MessageInfo *msg) {
+    print_system_message("User left the chat");
+    printf("Nickname: " CYAN "%s" RESET " (ID: " YELLOW "%d" RESET ")\n", msg->nickname,
+           server->clients[client_index].client_id);
+
+    MessageInfo leave_msg = *msg;
+    leave_msg.type = MSG_TYPE_SYSTEM;
+    sprintf(leave_msg.message, "%s left the chat", msg->nickname);
+    broadcast_message(server, &leave_msg, client_index);
+    return 1;
+}
+
+static int process_message(ServerState *server, int client_index, const MessageInfo *msg) {
+    switch (msg->type) {
+        case MSG_TYPE_JOIN:
+            return handle_join_message(server, client_index, msg);
+
+        case MSG_TYPE_CHAT:
+            print_message(msg->nickname, msg->message);
+            broadcast_message(server, msg, client_index);
+            return 0;
+
+        case MSG_TYPE_PRIVATE:
+            return handle_private_message(server, client_index, msg);
+
+        case MSG_TYPE_LEAVE:
+            return handle_leave_message(server, client_index, msg);
+
+        default:
+            print_error("Unknown message type received");
+            return 0;
+    }
+}
+
+void *handle_client(void *arg) {
+    client_thread_data_t *data = arg;
+    ServerState *server = data->server;
     int client_index = data->client_index;
     free(data);
 
@@ -295,7 +382,8 @@ void* handle_client(void* arg) {
     welcome_msg.timestamp = time(NULL);
     send_message(server->clients[client_index].socket, &welcome_msg);
 
-    while (1) {
+    int should_disconnect = 0;
+    while (!should_disconnect) {
         int bytes_received = receive_message(server->clients[client_index].socket, &msg);
 
         if (bytes_received <= 0) {
@@ -303,87 +391,17 @@ void* handle_client(void* arg) {
             break;
         }
 
-        switch (msg.type) {
-            case MSG_TYPE_JOIN: {
-                if (is_nickname_available(server, msg.nickname)) {
-                    if (register_nickname(server, msg.nickname, server->clients[client_index].client_id) == 0) {
-                        strcpy(server->clients[client_index].nickname, msg.nickname);
-                        print_system_message("User joined the chat");
-                        printf("Nickname: " CYAN "%s" RESET " (ID: " YELLOW "%d" RESET ")\n",
-                               msg.nickname, server->clients[client_index].client_id);
-
-                        MessageInfo success_msg;
-                        success_msg.type = MSG_TYPE_NICKNAME_AVAILABLE;
-                        strcpy(success_msg.nickname, "Server");
-                        sprintf(success_msg.message, "Nickname '%s' is registered!", msg.nickname);
-                        success_msg.timestamp = time(NULL);
-                        send_message(server->clients[client_index].socket, &success_msg);
-
-                        MessageInfo join_msg = msg;
-                        join_msg.type = MSG_TYPE_SYSTEM;
-                        sprintf(join_msg.message, "%s joined the chat", msg.nickname);
-                        broadcast_message(server, &join_msg, client_index);
-                    } else {
-                        MessageInfo error_msg;
-                        error_msg.type = MSG_TYPE_NICKNAME_TAKEN;
-                        strcpy(error_msg.nickname, "Server");
-                        strcpy(error_msg.message, "Failed to register nickname. Please try again.");
-                        error_msg.timestamp = time(NULL);
-                        send_message(server->clients[client_index].socket, &error_msg);
-                    }
-                } else {
-                    MessageInfo taken_msg;
-                    taken_msg.type = MSG_TYPE_NICKNAME_TAKEN;
-                    strcpy(taken_msg.nickname, "Server");
-                    sprintf(taken_msg.message, "Nickname '%s' is already taken. Please choose another.", msg.nickname);
-                    taken_msg.timestamp = time(NULL);
-                    send_message(server->clients[client_index].socket, &taken_msg);
-                }
-                break;
-            }
-
-            case MSG_TYPE_CHAT:
-                print_message(msg.nickname, msg.message);
-                broadcast_message(server, &msg, client_index);
-                break;
-
-            case MSG_TYPE_PRIVATE: {
-                printf(MAGENTA "[PRIVATE]" RESET " From " CYAN "%s" RESET " to " CYAN "%s" RESET ": %s\n",
-                       msg.nickname, msg.target_nickname, msg.message);
-
-                if (server_send_private_message(server, &msg) != 0) {
-                    MessageInfo error_msg;
-                    error_msg.type = MSG_TYPE_SYSTEM;
-                    strcpy(error_msg.nickname, "Server");
-                    sprintf(error_msg.message, "User '%s' not found or offline.", msg.target_nickname);
-                    error_msg.timestamp = time(NULL);
-                    send_message(server->clients[client_index].socket, &error_msg);
-                }
-                break;
-            }
-
-            case MSG_TYPE_LEAVE:
-                print_system_message("User left the chat");
-                printf("Nickname: " CYAN "%s" RESET " (ID: " YELLOW "%d" RESET ")\n",msg.nickname, server->clients[client_index].client_id);
-
-                MessageInfo leave_msg = msg;
-                leave_msg.type = MSG_TYPE_SYSTEM;
-                sprintf(leave_msg.message, "%s left the chat", msg.nickname);
-                broadcast_message(server, &leave_msg, client_index);
-                goto cleanup;
-
-            default:
-                print_error("Unknown message type received");
-                break;
+        int result = process_message(server, client_index, &msg);
+        if (result == 1) {
+            should_disconnect = 1;
         }
     }
 
-cleanup:
     remove_client(server, client_index);
     pthread_exit(NULL);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     int port = DEFAULT_PORT;
     ServerState server;
 
@@ -416,7 +434,13 @@ int main(int argc, char* argv[]) {
             continue;
         }
 
-        client_thread_data_t* thread_data = malloc(sizeof(client_thread_data_t));
+        client_thread_data_t *thread_data = malloc(sizeof(client_thread_data_t));
+        if (thread_data == NULL) {
+            print_error("Failed to allocate memory for thread data");
+            remove_client(&server, client_index);
+            continue;
+        }
+
         thread_data->server = &server;
         thread_data->client_index = client_index;
 
